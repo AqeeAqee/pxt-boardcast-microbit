3a60c24a989fbed29ef3fd37a8993bc07e61b299
AqeeAqee <aqee@msn.com>
2019-09-04 01:28:19 +0800

    initial

--- main.ts
+++ main.ts
@@ -1,7 +1,38 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+// Add your code here
+//% color="#eeaaaa"
+namespace broadcast {
+
+    export interface BroadcastHandler {
+        msg: string;
+        handler: () => void;
+    }
+
+    let broadcastHandlers: BroadcastHandler[];
+
+
+    /**
+     * Run following code, when received the same Message String
+     */
+    //% block="When I receive $msg"
+    //% blockAllowMultiple=1
+    export function onEventWithArgs(msg: string, handler: () => void) {
+        if (broadcastHandlers == null)
+            broadcastHandlers = [];
+        broadcastHandlers.push({ msg: msg, handler: handler });
+    }
+
+    /**
+     * Send out Broadcast with Message String
+     */
+    //% block="Broadcast %msg"
+    export function broadcast(msg: string) {
+        broadcastHandlers
+            .filter(h => h.msg == msg)
+            //for micro:bit, runInBackground deprecated in Arcade
+            .forEach(h => control.inBackground(h.handler));
+            //for Arcade
+            //.forEach(h => control.runInParallel(h.handler));
+    }
+
+}
+
--- test.ts
+++ test.ts
@@ -1,1 +1,17 @@
 // tests go here; this will not be compiled when this package is used as a library
+broadcast.onEventWithArgs("消息", function () {
+    basic.pause(200)
+    led.toggle(3, 1)
+})
+broadcast.onEventWithArgs("消息", function () {
+    led.toggle(1, 1)
+})
+broadcast.onEventWithArgs("消息", function () {
+    for (let i = 0; i < 1111; i++) {
+        basic.pause(1000)
+    }
+})
+basic.forever(function () {
+    broadcast.broadcast("消息")
+    basic.pause(1000)
+})
